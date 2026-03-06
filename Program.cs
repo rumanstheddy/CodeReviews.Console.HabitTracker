295d438bc09f0d7eda072a97a77cbb9548ecfacf
Sumanth Reddy <sumanthcob@gmail.com>
2026-03-06 12:26:06 -0500

    feat: create tables in SQLite database

--- Program.cs
+++ Program.cs
@@ -2,27 +2,44 @@
 // TODO: This habit can't be tracked by time (ex. hours of sleep), only by quantity (ex. number of water glasses a day)
 // TODO: Users need to be able to input the date of the occurrence of the habit
 // TODO: The application should store and retrieve data from a real database
-// TODO: When the application starts, it should create a sqlite database, if one isn’t present.
+// When the application starts, it should create a sqlite database, if one isn’t present.
 // TODO: It should also create a table in the database, where the habit will be logged.
 // TODO: The users should be able to insert, delete, update and view their logged habit.
 // TODO: You should handle all possible errors so that the application never crashes.
 // TODO: You can only interact with the database using ADO.NET.
 // TODO: Your project needs to contain a Read Me file where you'll explain how your app works and tell a little bit about your thought progress.
+// ? https://www.thecsharpacademy.com/project/12/habit-logger
+// ? https://reintech.io/blog/mastering-parameterized-queries-ado-net
 
 class Program
 {
     static void Main()
     {
-        string connectionString = "Data Source=habits.db";
+        string connectionString = "Data Source=habit_tracker.db";
         using var connection = new SqliteConnection(connectionString);
         connection.Open();
 
-        string tableCommand = @"CREATE TABLE IF NOT EXISTS Habits(
-                                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    Date TEXT NOT NULL,
-                                    Quantity INTEGER NOT NULL
+        string habitsTableCommand = @"CREATE TABLE IF NOT EXISTS habits(
+                                    habit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    habit_name TEXT NOT NULL CHECK(length(habit_name) <= 100)
                                 );";
-        using var createTable = new SqliteCommand(tableCommand, connection);
+
+        string unitsTableCommand = @"CREATE TABLE IF NOT EXISTS units(
+                                    unit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    unit_name TEXT NOT NULL CHECK(length(unit_name) <= 30)
+                                );";
+
+        string habitLogsTableCommand = @"CREATE TABLE IF NOT EXISTS habit_logs(
+                                    log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    date TEXT NOT NULL CHECK(date(date) IS NOT NULL),
+                                    habit_id INTEGER NOT NULL,
+                                    quantity INTEGER NOT NULL,
+                                    unit_id INTEGER NOT NULL,
+                                    FOREIGN KEY (habit_id) REFERENCES habits(habit_id),
+                                    FOREIGN KEY (unit_id) REFERENCES units(unit_id)                                    
+                                );";
+
+        using var createTable = new SqliteCommand(habitsTableCommand + unitsTableCommand + habitLogsTableCommand, connection);
         createTable.ExecuteNonQuery();
     }
 }
