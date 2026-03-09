f9ff8b7d474736f2823e0c0a79394d21c266d87a
Sumanth Reddy <sumanthcob@gmail.com>
2026-03-09 12:18:50 -0400

    feat: choose existing habit option from db

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // TODO: Users need to be able to input the date of the occurrence of the habit
 // TODO: The application should store and retrieve data from a real database
 // When the application starts, it should create a sqlite database, if one isn’t present.
-// TODO: It should also create a table in the database, where the habit will be logged.
+// It should also create a table in the database, where the habit will be logged.
 // TODO: The users should be able to insert, delete, update and view their logged habit.
 // TODO: You should handle all possible errors so that the application never crashes.
 // TODO: You can only interact with the database using ADO.NET.
@@ -21,12 +21,12 @@ class Program
 
         string habitsTableCommand = @"CREATE TABLE IF NOT EXISTS habits(
                                     habit_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    habit_name TEXT NOT NULL CHECK(length(habit_name) <= 100)
+                                    habit_name TEXT NOT NULL UNIQUE CHECK(length(habit_name) <= 100) COLLATE NOCASE
                                 );";
 
         string unitsTableCommand = @"CREATE TABLE IF NOT EXISTS units(
                                     unit_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    unit_name TEXT NOT NULL CHECK(length(unit_name) <= 30)
+                                    unit_name TEXT NOT NULL UNIQUE CHECK(length(unit_name) <= 30) COLLATE NOCASE
                                 );";
 
         string habitLogsTableCommand = @"CREATE TABLE IF NOT EXISTS habit_logs(
@@ -41,5 +41,62 @@ class Program
 
         using var createTable = new SqliteCommand(habitsTableCommand + unitsTableCommand + habitLogsTableCommand, connection);
         createTable.ExecuteNonQuery();
+
+
+        // string insertHabitCommand = @"INSERT INTO habits (habit_name) VALUES ('Drink water');";
+        // string insertUnitCommand = @"INSERT INTO units (unit_name) VALUES ('Glasses');";
+        // new SqliteCommand(insertHabitCommand + insertUnitCommand, connection).ExecuteNonQuery();
+
+        int habitId = -1;
+        int unitId = -1;
+        string dateLogged = "";
+
+        Console.WriteLine("Choose a habit to log: ");
+        string selectAllHabitsCommand = @"SELECT * FROM habits;";
+        using var reader = new SqliteCommand(selectAllHabitsCommand, connection).ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine($"{reader["habit_id"]} : {reader["habit_name"]}");
+        }
+
+        string selectHabitForIdCommand = @"SELECT COUNT(*) FROM habits WHERE habit_id = @HabitId;";
+
+        string? habitIdInput = Console.ReadLine();
+        while (string.IsNullOrEmpty(habitIdInput) || !int.TryParse(habitIdInput, out habitId) || habitId < 0)
+        {
+            Console.WriteLine("This is not a valid ID. Please try again: ");
+            habitIdInput = Console.ReadLine();
+        }
+
+        int habitCount = 0;
+        while (habitCount != 1)
+        {
+            using (var command = new SqliteCommand(selectHabitForIdCommand, connection))
+            {
+                command.Parameters.Add("@HabitId", SqliteType.Integer).Value = habitId;
+                habitCount = Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        Console.WriteLine(habitCount);
+
+        // if (Console.ReadLine() == "p")
+        // {
+        //     Console.WriteLine("Select which results you would like to use: \n");
+        //     for (int i = 0; i < calculator.ResultHistory.Count; i++)
+        //     {
+        //         Console.WriteLine($"\t{i + 1} - {calculator.ResultHistory[i]}");
+        //     }
+        //     Console.WriteLine();
+        //     Console.Write("Your option? ");
+        //     string? selectedResult = Console.ReadLine();
+        //     while (selectedResult == null || !int.TryParse(selectedResult, out int idx) || idx < 1 || idx > calculator.ResultHistory.Count)
+        //     {
+        //         Console.Write("This is not valid input. Please select an appropriate option: ");
+        //         selectedResult = Console.ReadLine();
+        //     }
+        //     cleanNum1 = calculator.ResultHistory[int.Parse(selectedResult) - 1];
+        //     usePreviousResult = true;
+        // }
     }
 }
